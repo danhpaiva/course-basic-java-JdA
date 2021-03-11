2a6b1b47a3dc29cb945f3dd38d31796cfda7ab43
Paiva <danpaiva@live.com>
2021-03-11 16:41:29 -0300

    Hello World in Eclipse

---
+++ class/Curso de Java/src/cursoJava/Hello.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package cursoJava;
+
+/**
+ * @author Daniel Paiva
+ *
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
