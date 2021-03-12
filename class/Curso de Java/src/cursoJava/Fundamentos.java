268148407b65bfe0144e7ed2d491931cdbf35238
Paiva <danpaiva@live.com>
2021-03-12 08:33:15 -0300

    Fundamento PT01

---
+++ class/Curso de Java/src/cursoJava/Fundamentos.java
@@ -0,0 +1,17 @@
+package cursoJava;
+
+public class Fundamentos {
+	public static void main(String[] args) {
+		String nome;
+		nome = "Paiva";
+		int idade = 28;
+		char sexo = 'M';
+		double altura = 1.68;
+		boolean vivo = true;
+		System.out.println("Nome: " + nome);
+		System.out.println("Idade: " + idade);
+		System.out.println("Sexo: " + sexo);
+		System.out.println("Altura: " + altura);
+		System.out.println("Vivo: " + vivo);
+	}
+}
