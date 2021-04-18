b5b2f26f88bdad2e553616b998019a9ce5131710
Paiva <danpaiva@live.com>
2021-04-18 11:06:20 -0300

    Class06 | Done

---
+++ class/CursoDeJava/src/cursoJava/Class06.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package cursoJava;
+
+/**
+ * @author danhpaiva
+ *
+ */
+public class Class06 {
+
+	public static void main(String[] args) {
+		String nome = "Daniel Paiva";
+		int idade = 28;
+		char sexo = 'M';
+		double altura = 1.70;
+		boolean vivo = true;
+		System.out.println(
+				"Nome: " + nome + "\nIdade: " + idade + "\nSexo: " + sexo + "\nAltura: " + altura + "\nVivo: " + vivo);
+	}
+
+}
