1767f32cf5fb45846c9e32542357964a443f0ff7
Paiva <danpaiva@live.com>
2021-04-18 10:54:19 -0300

    Class05 | Done

---
+++ class/CursoDeJava/src/cursoJava/Class05.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package cursoJava;
+
+/**
+ * @author danhpaiva
+ * Hello World in Java
+ */
+public class Class05 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
