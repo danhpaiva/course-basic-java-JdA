8da71bec35b70477edc6c1a7fd524cb8552a81aa
Paiva <danpaiva@live.com>
2021-04-18 12:41:05 -0300

    Class11 | Done

---
+++ class/CursoDeJava/src/poo/Aviao.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package poo;
+
+/**
+ * @author danhpaiva
+ *
+ */
+public class Aviao extends Carro{
+	double envergadura;
+	
+	void aterrizar() {
+		System.out.println("-----_____");
+	}
+
+}
---
+++ class/CursoDeJava/src/poo/Cessna.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package poo;
+
+/**
+ * @author danhpaiva
+ *
+ */
+public class Cessna {
+
+	public static void main(String[] args) {
+		Aviao cessna = new Aviao();
+
+		cessna.ano = 2000;
+		cessna.cor = "cinza";
+		cessna.envergadura = 11;
+		System.out.println("Avião Cessna");
+		System.out.println("Ano: " + cessna.ano);
+		System.out.println("Cor: " + cessna.cor);
+		System.out.println("Envergadura: " + cessna.envergadura);
+		cessna.aterrizar();
+		cessna.desligar();
+	}
+
+}
