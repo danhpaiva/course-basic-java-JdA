78cc3353a08c57812c38183537eda2df8ef18076
Paiva <danpaiva@live.com>
2021-04-18 12:05:18 -0300

    Class09 | Done

--- class/CursoDeJava/README.md
+++ class/CursoDeJava/README.md
@@ -3,8 +3,9 @@
 <p>About:</p>
 
 - Exercise Class 05 - Hello World
-- Exercise Class 06 - Fundamentals - Variables - Part 01
-- Exercise Class 07 - Arithmetic Operators - Variables - Part 02
-- Exercise Class 08 - Conditional structures | Switch - Part 04
+- Exercise Class 06 - Fundamentals - Variables - Part 01/05
+- Exercise Class 07 - Arithmetic Operators - Variables - Part 02/05
+- Exercise Class 08 - Conditional structures | Switch - Part 04/05
+- Exercise Class 09 - For | While | Do While - Part 05/05
 
 <p>Exercises were performed using the Eclipse IDE</p>
---
+++ class/CursoDeJava/src/cursoJava/Class09.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package cursoJava;
+
+/**
+ * @author danhpaiva
+ *
+ */
+public class Class09 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Uso do FOR");
+
+		for (int i = 0; i <= 10; i++)
+			System.out.println("Valor do i: " + i);
+
+		System.out.println("\nTabuada de 10");
+		for (int i = 0; i <= 10; i++)
+			System.out.println("10 * " + i + " = " + (10 * i));
+
+		System.out.println("\nUso do While");
+		double valor = 10.1;
+		while (valor < 11.5) {
+			System.out.println("Valor:" + valor);
+			valor += 0.3;
+		}
+
+		System.out.println("\nUso do DOWhile");
+		double preco = 10;
+		do {
+			System.out.println("Preço: " + preco);
+			preco += 0.2;
+		} while (preco < 11);
+
+	}
+
+}
