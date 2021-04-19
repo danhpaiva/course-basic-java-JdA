a61fc19b68c01ff86eca84c38cc7532fa03ee99c
Paiva <danpaiva@live.com>
2021-04-19 12:19:45 -0300

    Add Exercise Porcentagem

---
+++ Exercises/src/exercise2/Programa.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package exercise2;
+
+import java.util.Scanner;
+
+/**
+ * @author danhpaiva
+ *
+ */
+public class Programa {
+
+	public static void main(String[] args) {
+		System.out.println("Calculo de Porcentagem\n");
+
+		System.out.print("Digite o valor: ");
+		Scanner sc = new Scanner(System.in);
+		Porcentagem porcentagem = new Porcentagem();
+		porcentagem.setY(sc.nextDouble());
+
+		System.out.print("Agora digite o valor da porcentagem em cima de " + porcentagem.getY() + ": ");
+		porcentagem.setX(sc.nextDouble());
+		sc.close();
+
+		System.out.println("O valor da porcentagem é: " + porcentagem.calcularPorcentagem(porcentagem));
+	}
+
+}
