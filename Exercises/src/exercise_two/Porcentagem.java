9ce386d179f2f75cbcb8abb55a76a40d485ae8c4
Paiva <danpaiva@live.com>
2021-04-19 20:09:22 -0300

    update packege name

---
+++ Exercises/src/exercise_two/Porcentagem.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package exercise_two;
+
+/**
+ * @author danhpaiva
+ *
+ */
+public class Porcentagem {
+	private double x;
+	private double y;
+	private double valor;
+
+	public double getX() {
+		return x;
+	}
+
+	public double setX(double x) {
+		this.x = x;
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public double setY(double y) {
+		this.y = y;
+		return y;
+	}
+
+	public double calcularPorcentagem(Porcentagem p) {
+		p.valor = (p.x * p.y) / 100;
+		return p.valor;
+	}
+}
